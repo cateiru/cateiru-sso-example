e4488f6d51042175e6f145393deac39c3d82cfbb
Yuto Watanabe <yuto.w51942@gmail.com>
2022-02-17 21:58:34 +0900

    修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,18 @@
 import type { NextPage } from 'next'
-import { Button, Center } from '@chakra-ui/react'
+import { Button, Center, Box, Heading } from '@chakra-ui/react'
 import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <Center height="100vh">
-      <Link href="/api/login" passHref>
-        <Button>ログイン</Button>
-      </Link>
+      <Box>
+        <Heading>CateiruSSOログインえぐざんぷる</Heading>
+        <Center mt="1rem">
+          <Link href="/api/login" passHref>
+            <Button>ログイン</Button>
+          </Link>
+        </Center>
+      </Box>
     </Center>
   )
 }
